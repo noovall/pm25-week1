f322f2395a471b2a1f6918a8066b35b9ae7bec9d
noovall <laluadip@gmail.com>
2025-03-05 21:38:25 +0800

    assignment 1 commit hello app

---
+++ Tugas1_SayHelloApp/app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.helloapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val addName : EditText = findViewById(R.id.addName)
+        val button : Button = findViewById(R.id.button)
+        val hasil : TextView = findViewById(R.id.hasil)
+
+        button.setOnClickListener {
+            var str : String = addName.text.toString()
+            hasil.setText("Hello, $str !")
+        }
+    }
+}
